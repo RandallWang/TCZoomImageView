111ee9cc8e03b47e7773cfb0f2cd3b025d903562
wangtianchi <wangtianchi@nutstore.net>
2018-05-05 17:55:29 +0800

    Initial Commit

---
+++ TCZoomingImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TCZoomingImageView
+//
+//  Created by Tianchi Wang on 2018/5/5.
+//  Copyright © 2018年 Tianchi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
