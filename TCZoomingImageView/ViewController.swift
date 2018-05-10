e1871d935b6199632183fc86652ebb5c697982ad
wangtianchi <wangtianchi@nutstore.net>
2018-05-10 10:17:26 +0800

    finish V1.0

--- TCZoomingImageView/ViewController.swift
+++ TCZoomingImageView/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zoomView: ZoomingImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        2018-02-08_19-43-10.jpg
+//        2018-01-01_10-17-26.jpg
+//        2015-06-28_21-58-50.jpg
+//        24317.jpg
+        if let testImage = UIImage.init(named: "2018-01-01_10-17-26.jpg", in: Bundle.main, compatibleWith: nil) {
+            zoomView.setUpImageView(withImage: testImage)
+        }
     }
 
     override func didReceiveMemoryWarning() {
